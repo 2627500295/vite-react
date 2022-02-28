b74a3a2b1d1f16fc206a003fcef9301ce6d99498
2627500295 <2627500295@qq.com>
2022-02-28 11:31:48 +0800

    1 file modified

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,18 @@ import { createRoot } from 'react-dom';
 
 import App from './containers/App';
 
+import reportWebVitals from './reportWebVitals';
+
 import './assets/styles/index.scss';
 
 const container = document.getElementById('root')!;
 
+// ReactDOM.render is no longer supported in React 18. Use createRoot instead.
+// Until you switch to the new API, your app will behave as if it's running React 17.
+// Learn more: https://reactjs.org/link/switch-to-createroot
 createRoot(container).render(<App />);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
